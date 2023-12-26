55dd9304c094ba7865e1a93682c87bc427b95c1b
rohitashgoswami <rohitash0510@gmail.com>
2023-12-26 13:18:37 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//.>>> Epesode 1.  >>>>>
+
+/*
+var n = 2;
+function square (num) {
+    var ans = num*num;
+    return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+var square = square(7);
+console.log(square7);     */
+
+
+// >>>> Epesode 2. >>>>> 
+
+getName();
+console.log(x);
+console.log(getName);
+var x = 7;
+
+function getName(){
+    console.log("Namaste Javascrip");
+}
